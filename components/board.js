0b1b4acbc7cea51006e3099615adadb22f9da28c
Leonidas Esteban <leonidasesteban@gmail.com>
2022-10-08 01:49:08 -0500

    clase-11

--- components/board.js
+++ components/board.js
@@ -5,9 +5,22 @@ import { todoList, inProgressList, doneList } from "./data"
 
 function Board() {
   const [dragged, setDragged] = useState(null)
+  const [listOfLists, setListOfLists] = useState({
+    todoList,
+    inProgressList,
+    doneList
+  })
   function handleDrop(event) {
     const list = event.currentTarget.dataset.id
-    console.log(dragged)
+    const listOfListsClone = structuredClone(listOfLists)
+
+    const newList = listOfListsClone[dragged.list].filter(item => item.id !== dragged.data.id)
+
+    listOfListsClone[dragged.list] = newList
+    listOfListsClone[list].push(dragged.data)
+
+
+    setListOfLists(listOfListsClone)
   }
   return (
     <div className="flex flex-col flex-1 gap-4 p-4 ">
@@ -17,25 +30,25 @@ function Board() {
         </h1>
       </div>
       <main className="flex flex-1 gap-6 ">
-        <List title="TODO" handleDrop={handleDrop} id="todo">
+        <List title="TODO" handleDrop={handleDrop} id="todoList">
           {
-            todoList.map(item => (
+            listOfLists.todoList.map(item => (
               <Card {...item} key={item.id} setDragged={setDragged} />
             ))
           }
 
         </List>
-        <List title="In Progress" handleDrop={handleDrop} id="in-progress">
+        <List title="In Progress" handleDrop={handleDrop} id="inProgressList">
           {
-            inProgressList.map(item => (
+            listOfLists.inProgressList.map(item => (
               <Card {...item} key={item.id} setDragged={setDragged} />
             ))
           }
 
         </List>
-        <List title="Done" handleDrop={handleDrop} id="done">
+        <List title="Done" handleDrop={handleDrop} id="doneList">
           {
-            doneList.map(item => (
+            listOfLists.doneList.map(item => (
               <Card {...item} key={item.id} setDragged={setDragged} />
             ))
           }
